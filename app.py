1ad4dbbce786b1104f2eafe84f685113922767e8
CassioFonseca001 <cassioasf01@gmail.com>
2025-03-06 17:47:35 -0300

    Correcao porta

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def webhook():
 #    app.run(host='0.0.0.0', port=5001, debug=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
